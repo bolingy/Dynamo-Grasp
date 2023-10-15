b639ea34359390e7c105d4a5515ab8cabf07ebdc
Markus Grotz <grotz@uw.edu>
2023-10-15 16:02:48 -0700

    add data_path as option for data_collection.py CLI

--- data_collection.py
+++ data_collection.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import isaacgym
 import isaacgymenvs
+import os
+from datetime import datetime
+import random
+import string
+
+
 import torch
 
 import click
-import os
+from click_prompt import filepath_option
 
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
@@ -12,9 +18,6 @@ os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
 
 def get_data_path():
-    from datetime import datetime
-    import random
-    import string
 
     datetime_string = datetime.now().isoformat().replace(":", "")[:-7]
     random_string = "".join(random.choice(string.ascii_letters) for _ in range(6))
@@ -26,7 +29,8 @@ def get_data_path():
 
 @click.command()
 @click.option("--num-envs", default=1)
-def generate(num_envs):
+@filepath_option("--data-path", default=get_data_path())
+def generate(num_envs: int, data_path: str):
     envs = isaacgymenvs.make(
         seed=0,
         task="UR16eManipulation",
@@ -36,7 +40,7 @@ def generate(num_envs):
         multi_gpu=True,
         headless=False,
         graphics_device_id=0,
-        data_path=get_data_path(),
+        data_path=data_path,
     )
     obs = envs.reset()
 
